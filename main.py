ab69bdd25fa99ab0cf11bbf511b1f31ed7acd166
analystrusso <adam.jordan.russo@gmail.com>
2024-08-14 11:20:52 -0400

    altered the print(word count) line to say Word count: (wordcount)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     book_path = "github.com/analystrusso/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
     print(text)
-    print(countwords(text))
+    print("Word count:", countwords(text))
     
 def countwords(text):
     wordcount = len(text.split())

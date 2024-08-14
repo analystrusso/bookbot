5216396fabaa76111adf95aaf5d45a489e884963
analystrusso <adam.jordan.russo@gmail.com>
2024-08-14 11:16:53 -0400

    added the def countwords(text) function to main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ def main():
     book_path = "github.com/analystrusso/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
     print(text)
-
-
+    print(countwords(text))
+    
+def countwords(text):
+    wordcount = len(text.split())
+    return wordcount
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-
-
 
 main()
